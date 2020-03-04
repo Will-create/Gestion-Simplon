5e4f6521304ff0629b8569e5666099890e699f51
Louis <louisbertson@gmail.com>
2020-03-04 23:57:17 +0000

    depot initial du projet

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo 'bonjour le monde';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Accueil-Gestion-Simplon</title>
+</head>
+<body>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo id quibusdam, vero facere illum maiores laudantium natus assumenda itaque quam aspernatur sunt, voluptate odio cumque voluptatum dolores porro provident iste.</p>
+</body>
+</html>
