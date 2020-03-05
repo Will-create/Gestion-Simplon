2d648807ff06aa62d5b91f21e3bf0cd78593b519
Louis <louisbertson@gmail.com>
2020-03-05 16:06:52 +0000

    page d'accueil terminée

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
-<?php
-echo 'bonjour le monde';
-?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script type="text/javascript" src="js/jquery/jquery.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     <title>Accueil-Gestion-Simplon</title>
 </head>
 <body>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo id quibusdam, vero facere illum maiores laudantium natus assumenda itaque quam aspernatur sunt, voluptate odio cumque voluptatum dolores porro provident iste.</p>
+<div class="container">
+	<nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#"><img class="logo" src="images/logo.png"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="menu-nav">
+      <li id="actif"><a href="#">ACCUEIL</a></li>
+      <li ><a href="#">APPRENANTS</a></li>
+      <li><a href="#">TUTEURS</a></li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input id="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    </form>
+  </div>
+</nav>
+
+<h1 id="bienvenu">Bienvenu dans Gestion des apprenants</h1>
+<div id="banniere">
+	<img src="images/banniere.jpg">
+</div>
+<div id="footer">
+	<h4 style="color:white;">COPYRIGHT SIMPLON 2020</h4>
+</div>
+</div>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
