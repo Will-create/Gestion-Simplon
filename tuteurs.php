fad33a37aa3473cd7be2d4610b85ef39e29f6f7d
Saba Emilie <emiliesaba6@gmail.com>
2020-03-06 18:43:32 +0000

    la partie liste des tuteurs est terminée

--- tuteurs.php
+++ tuteurs.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <script type="text/javascript" src="js/jquery/jquery.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto">
     <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/tuteurs.css">
     <title>Accueil-Gestion-Simplon</title>
 </head>
 <body>
@@ -18,9 +20,9 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="menu-nav">
-      <li id="actif"><a href="#">ACCUEIL</a></li>
+      <li ><a href="#">ACCUEIL</a></li>
       <li ><a href="#">APPRENANTS</a></li>
-      <li><a href="#">TUTEURS</a></li>
+      <li id="actif"><a href="#">TUTEURS</a></li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input id="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -28,7 +30,51 @@
   </div>
 </nav>
 <div id="global">
-  <div class="fils" id="liste"></div>
+  <div class="fils" id="liste"> 
+  <h1 id="bienvenu" id="p2">Liste des Tuteurs</h1>
+<div class="table-responsive-sm">
+<table class="table stable table-bordered">
+  <thead class="stable">
+    <tr  class="stable1" >
+      <th scope="col">N°</th>
+      <th scope="col">Nom</th>
+      <th scope="col">Prénom</th>
+      <th scope="col">Profession</th>
+      <th scope="col">Contact</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>OUEDRAOGO</td>
+      <td>Salif</td>
+      <td>Douanier</td>
+      <td>00593215</td>
+
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>NABALOUM</td>
+      <td>Ali</td>
+      <td>Enseignant</td>
+      <td>00632255</td>
+    </tr>
+    <tr>
+      <td >3</td>
+      <td>BEOGO </td>
+      <td>Louis</td>
+      <td>Musicien</td>
+      <td>03212255</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<div class="sbouton1"> 
+<button type="button" class="btn btn-danger sbouton2">Imprimer</button>
+<button type="button" class="btn btn-danger sbouton2">Nouveau</button>
+</div>
+  
+  </div>
   <div class="fils" id="formulaire"></div>
    <div class="fils" id="details"></div>
 </div>
